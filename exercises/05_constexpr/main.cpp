5a3eff1a3a25e91d289fb8b655c0dc4164dbf67c
between0and1 <>
2025-01-04 15:06:19 +0800

    preparing for switch to rust

--- exercises/05_constexpr/main.cpp
+++ exercises/05_constexpr/main.cpp
@@ -1,6 +1,6 @@
 #include "../exercise.h"
 
-unsigned long long fibonacci(int i) {
+constexpr unsigned long long fibonacci(int i) {
     switch (i) {
         case 0:
             return 0;
@@ -12,14 +12,14 @@ unsigned long long fibonacci(int i) {
 }
 
 int main(int argc, char **argv) {
-    auto FIB20 = fibonacci(20);
+    constexpr auto FIB20 = fibonacci(20);
     ASSERT(FIB20 == 6765, "fibonacci(20) should be 6765");
     std::cout << "fibonacci(20) = " << FIB20 << std::endl;
 
     // TODO: 观察错误信息，修改一处，使代码编译运行
     // PS: 编译运行，但是不一定能算出结果……
-    auto ANS_N = 90;
-    auto ANS = fibonacci(ANS_N);
+    constexpr auto ANS_N = 90;
+    constexpr auto ANS = fibonacci(ANS_N);
     std::cout << "fibonacci(" << ANS_N << ") = " << ANS << std::endl;
 
     return 0;
